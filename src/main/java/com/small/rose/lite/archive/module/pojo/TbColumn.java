99fe5e732de09d06a05db675fdc01f7a437d71bb
small-rose <small-rose@qq.com>
2023-11-20 00:25:32 +0800

    :ambulance: 临时类

---
+++ src/main/java/com/small/rose/lite/archive/module/pojo/TbColumn.java
@@ -0,0 +1,12 @@
+package com.small.rose.lite.archive.module.pojo;
+
+import lombok.Data;
+
+@Data
+public
+class TbColumn {
+
+    private String tableName ;
+    private String columnName ;
+    private String dataType ;
+}
